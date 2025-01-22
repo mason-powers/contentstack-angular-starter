d9d25f4228e88897188814c826646ca259120278
mason_powers <mpowers@pepboys.com>
2025-01-22 12:56:34 -0500

    Added env stuff

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,15 +1,17 @@
+
 export const environment = {
-  production: true,
+  production: true
 };
 
 export const Config = {
-  api_key: '',
-  delivery_token: '',
-  environment: '',
-  branch: '',
-  region: '',
-  management_token: '',
-  api_host: '',
-  app_host: '',
-  live_preview: true,
+  api_key: 'bltef5ffe8cd879aa8e',
+  delivery_token: 'csab7dcc6e68628b97ebcd1931',
+  environment: 'development',
+  branch: 'main',
+  region: 'us',
+  preview_token:'cs44e04ea36a2d0d6a152cff22',
+  preview_host:'rest-preview.contentstack.com',
+  api_host:'undefined',
+  app_host:'app.contentstack.com',
+  live_preview:true
 };
